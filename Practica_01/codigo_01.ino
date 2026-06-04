5ddedfdaa33256c38cf6afd2ac3a39ce1362a25d
Mario Alomoto <31331193+AloMario@users.noreply.github.com>
2026-06-04 15:34:11 -0500

    Update codigo_01.ino

--- Practica_01/codigo_01.ino
+++ Practica_01/codigo_01.ino
@@ -1,1 +1,15 @@
+const int sensorGas = 7;
+
+void setup() {
+  pinMode(sensorGas, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  int valor_sensor = digitalRead(sensorGas);
+  Serial.print("Sensor: ");
+  Serial.println(valor_sensor);
+  delay(1000);
+
+}
 
