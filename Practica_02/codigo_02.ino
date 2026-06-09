9c223500168998a2a682fc19fff894d91abbb07f
Mario Alomoto <31331193+AloMario@users.noreply.github.com>
2026-06-09 09:15:44 -0500

    Update codigo_02.ino

--- Practica_02/codigo_02.ino
+++ Practica_02/codigo_02.ino
@@ -1,1 +1,12 @@
+const int sensor = A0;
 
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  int valor_sensor = analogRead(sensor);
+  Serial.print("Sensor: ");
+  Serial.println(valor_sensor);
+  delay(1000);
+}
