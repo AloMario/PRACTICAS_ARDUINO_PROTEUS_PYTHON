44fcaa0bbf27abf346525bd485506bc03224a90d
Mario Alomoto <31331193+AloMario@users.noreply.github.com>
2026-06-08 21:45:13 -0500

    Add ultrasonic distance measurement code

--- Practica_03/codigo_03.ino
+++ Practica_03/codigo_03.ino
@@ -1,1 +1,29 @@
+const int trigger = 7;
+const int echo = 6;
+long duracion;
+float distancia;
 
+void setup() {
+  pinMode(trigger, OUTPUT);
+  pinMode(echo, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // Generar pulso ultrasónico
+  digitalWrite(trigger, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigger, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigger, LOW);
+
+  // Medir tiempo de retorno
+  duracion = pulseIn(echo, HIGH);
+
+  // Calcular distancia en centímetros
+  distancia = duracion * 0.0343 / 2;
+  // Mostrar resultado
+  Serial.print("Distancia: ");
+  Serial.println(distancia);
+  delay(1000);
+}
