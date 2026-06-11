d8411589a9ce23eb13c00f08083185a0434e4489
Mario Alomoto <31331193+AloMario@users.noreply.github.com>
2026-06-11 09:52:13 -0500

    Update codigo_07.ino

--- Practica_07/codigo_07.ino
+++ Practica_07/codigo_07.ino
@@ -1,1 +1,45 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+const int potenciometro = A1;
+const int echo = 8;
+const int trigger = 9;
+
+long duracion;
+float distancia;
+int valorPot;
+
+void setup() {
+  pinMode(trigger, OUTPUT);
+  pinMode(echo, INPUT);
+
+  lcd.init();
+  lcd.backlight();
+}
+
+void loop() {
+  // Lectura del potenciómetro
+  valorPot = analogRead(potenciometro);
+
+  // Lectura del sensor ultrasónico
+  digitalWrite(trigger, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigger, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigger, LOW);
+  duracion = pulseIn(echo, HIGH);
+  distancia = duracion * 0.0343 / 2;
+
+  // Mostrar información en LCD
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Pot:");
+  lcd.print(valorPot);
+  lcd.setCursor(0, 1);
+  lcd.print("Dist:");
+  lcd.print(distancia, 1);
+  lcd.print("cm");
+  delay(500);
+}
